1fa3bafd54332b53de7cfa538ff227353156387d
Matisse Mallette <matissemallette@gmail.com>
2022-08-30 11:09:40 -0700

    fill board_spec.rb and board.rb with code

---
+++ spec/board_spec.rb
@@ -0,0 +1,12 @@
+require 'RSpec'
+require './lib/board'
+
+RSpec.describe Board do
+    before(:each) do
+        @board = Board.new
+    end
+
+    it 'exists' do 
+        expect(@board).to be_an_instance_of(Board)
+    end
+end
