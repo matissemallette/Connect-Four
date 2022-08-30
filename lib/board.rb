8db1165b70b26b9de1838e37e9c0172c4e5c7a25
Matisse Mallette <matissemallette@gmail.com>
2022-08-30 14:38:53 -0700

    write cell.rb and cell_spec.rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,11 @@
-class Board 
-    def initialize
-    end
+require 'pry'
+
+class Board
+	def initialize
+  	@contents = Array.new(7) { Array.new(6) { |i| i = '.' } }
+  end
+
+  def render
+    p @contents + "\n"
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,11 +2,12 @@ require 'RSpec'
 require './lib/board'
 
 RSpec.describe Board do
-    before(:each) do
-        @board = Board.new
-    end
+  before(:each) do
+    @board = Board.new
+  end
 
-    it 'exists' do 
-        expect(@board).to be_an_instance_of(Board)
-    end
+  it 'exists' do 
+    expect(@board).to be_an_instance_of(Board)
+  end
+
 end
