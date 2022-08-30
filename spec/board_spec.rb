661bf84caf1ca7c41b92cf9517c3902757b7b837
Matisse Mallette <matissemallette@gmail.com>
2022-08-30 15:13:42 -0700

    create the barebones board_spec.rb and board.rb

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,6 @@
 require 'RSpec'
 require './lib/board'
+require './lib/cell'
 
 RSpec.describe Board do
   before(:each) do
@@ -10,4 +11,18 @@ RSpec.describe Board do
     expect(@board).to be_an_instance_of(Board)
   end
 
+  it 'has contents' do
+    expect(@board.contents).to be_an_instance_of(Array)
+  end
+
+	it 'can populate board' do
+		board2 = @board
+		board2.populate_board
+		expect(board2.contents[0][0]).to be_an_instance_of(Cell)
+	end
+
+  it 'can render contents' do
+		@board.populate_board
+		@board.render
+	end
 end
