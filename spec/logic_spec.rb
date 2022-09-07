8f8ae73e1d67b51fd9dbbe1d5cfc0f7d165ec799
Ashley <ashuhleyt@gmail.com>
2022-09-07 14:34:04 -0700

    refactored logic spec

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -6,89 +6,77 @@ require './lib/logic'
 RSpec.describe Logic do 
   before(:each) do 
     @logic = Logic.new
+    @board = Board.new
   end
   it 'exists' do 
     expect(@logic).to be_instance_of Logic
   end
 
   it 'will detect a horizontal win' do 
-    board = Board.new
-    board.populate_board
-    board.drop('a', 'x')
-    board.drop('b', 'x')
-    board.drop('c', 'x')
-    board.drop('d', 'x')
-    board.render
+    @board.populate_board
+    ['a', 'b', 'c', 'd'].each {|column| @board.drop(column, 'x')}
+  
+    @board.render
 
-    expect(@logic.horizontal_win(board, 'x')).to eq(true)
+    expect(@logic.horizontal_win(@board, 'x')).to eq(true)
 
-    board2 = Board.new
-    board2.populate_board
-    board2.drop('a', 'x')
-    board2.drop('c', 'x')
-    board2.drop('e', 'x')
-    board2.drop('g', 'x')
+    
+    @board.populate_board
+    ['a', 'c', 'e', 'g'].each {|column| @board.drop(column, 'x')}
 
-    expect(@logic.horizontal_win(board2, 'x')).to eq(false)
+    expect(@logic.horizontal_win(@board, 'x')).to eq(false)
   end 
 
   it 'will detect a vertical win' do 
-    board = Board.new
-    board.populate_board
-    4.times {board.drop('a', 'x')}
-    board.render
+   
+    @board.populate_board
+    4.times {@board.drop('a', 'x')}
+    @board.render
 
-    expect(@logic.vertical_win(board, 'x')).to eq(true)
+    expect(@logic.vertical_win(@board, 'x')).to eq(true)
 
-    board2 = Board.new
-    board2.populate_board
-    board2.drop('a', 'x')
-    board2.drop('b', 'x')
-    board2.drop('b', 'x')
-    board2.drop('a', 'x')
-    board2.render
+    
+    @board.populate_board
+    ['a', 'b', 'b', 'a'].each {|column| @board.drop(column, 'x')}
+    @board.render
 
-    expect(@logic.vertical_win(board2, 'x')).to eq(false)
+    expect(@logic.vertical_win(@board, 'x')).to eq(false)
   end
 
   it 'can detect if robot wins veritcally' do 
-    board = Board.new
-    board.populate_board
-    4.times {board.drop('a', 'o')}
-    board.render
+    
+    @board.populate_board
+    4.times {@board.drop('a', 'o')}
+    @board.render
 
-    expect(@logic.vertical_win(board, 'o')).to eq(true)
+    expect(@logic.vertical_win(@board, 'o')).to eq(true)
   end
 
   it 'can detect if robot wins horizontally' do 
-    board = Board.new
-    board.populate_board
-    board.drop('a', 'o')
-    board.drop('b', 'o')
-    board.drop('c', 'o')
-    board.drop('d', 'o')
-    board.render
+    
+    @board.populate_board
+    ['a', 'b', 'c', 'd'].each {|column| @board.drop(column, 'o')}
+    @board.render
 
-    expect(@logic.horizontal_win(board, 'o')).to eq(true)
+    expect(@logic.horizontal_win(@board, 'o')).to eq(true)
   end
 
   it 'can detect if player wins diagonally' do
-    board = Board.new
-    board.populate_board
-    board.drop('a', 'x')
-    board.drop('b', 'o')
-    board.drop('b', 'x')
-    2.times { board.drop('c', 'o') }
-    board.drop('c', 'x')
-    3.times { board.drop('d', 'o') }
-    board.drop('d', 'x')
-    board.render
+    @board.populate_board
+    @board.drop('a', 'x')
+    @board.drop('b', 'o')
+    @board.drop('b', 'x')
+    2.times { @board.drop('c', 'o') }
+    @board.drop('c', 'x')
+    3.times { @board.drop('d', 'o') }
+    @board.drop('d', 'x')
+    @board.render
 
-    expect(@logic.diagonal_win(board, 'x')).to eq(true)
+    expect(@logic.diagonal_win(@board, 'x')).to eq(true)
 
     mirrored_board = Board.new 
     mirrored_board.populate_board
-    mirrored_board.contents = board.mirrored_contents
+    mirrored_board.contents = @board.mirrored_contents
     mirrored_board.render 
 
     expect(@logic.diagonal_win(mirrored_board, 'x')).to eq(true)
