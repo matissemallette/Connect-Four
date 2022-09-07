e48c73c6931807961b94a938f33ce3fd3e053961
Matisse Mallette <matissemallette@gmail.com>
2022-09-07 14:22:16 -0700

    refactored board_spec.rb

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,9 +15,8 @@ RSpec.describe Board do
   end
 
 	it 'can populate board' do
-		board2 = @board
-		board2.populate_board
-		expect(board2.contents[0][0]).to be_an_instance_of(String)
+		@board.populate_board
+		expect(@board.contents[4][4]).to eq('.')
 	end
 
   it 'can render contents' do
@@ -26,20 +25,15 @@ RSpec.describe Board do
 	end
 
 	it 'can check contents of cell' do
-		board2 = @board 
-		board2.populate_board 
-		board2.contents[2][2] = 'X'
-		
-		expect(board2.is_occupied?(2, 2)).to eq(true)
+		@board.contents[2][2] = 'X'
+		expect(@board.is_occupied?(2, 2)).to eq(true)
 	end
 
 	it 'can have chips dropped in it' do
-		board2 = @board
-		board2.populate_board
-		board2.drop('A', 'X')
-		board2.drop('C', 'X')
-		board2.render
-		expect(board2.contents[5][0]).to eq('X')
+		@board.populate_board
+		@board.drop('A', 'X')
+		@board.drop('C', 'X')
+		expect(@board.contents[5][0]).to eq('X')
 	end
 
 	it 'has letter-to-column helper method' do
@@ -50,22 +44,12 @@ RSpec.describe Board do
 	end
 
 	it 'tells us if board is full' do
-    board2 = @board
-		board2.populate_board
+		@board.populate_board
     6.times do
-      board2.drop('a', 'O')
-      board2.drop('b', 'O')
-      board2.drop('c', 'O')
-      board2.drop('d', 'O')
-      board2.drop('e', 'O')
-      board2.drop('f', 'O')
-      board2.drop('g', 'O')
+			['a','b','c','d','e','f','g'].each {|column| @board.drop(column, 'O')}
     end
-    expect(board2.is_full?).to eq(true)
-
-		board3 = @board 
-		board3.populate_board
-
-		expect(board3.is_full?).to eq(false)
+    expect(@board.is_full?).to eq(true)
+		@board.populate_board
+		expect(@board.is_full?).to eq(false)
   end
 end
